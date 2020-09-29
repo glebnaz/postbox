c4b1c10fca9c8b5b2d1c2bd4d2f39768bfd1d107
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-09-29 12:05:38 +0400

    Init App

---
+++ cmd/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+//Config contains a link to a database connection, port settings
+type Config struct {
+	PORT        string `envconfig:"PORT" default:":7755"`
+	User        string `envconfig:"USER"`
+	Pass        string `envconfig:"PASS"`
+	TemplateDir string `envconfig:"TEMP_DIR" default:"emails"`
+}
+
+//Init initializes config in App
+func (c *Config) Init() {
+	err := envconfig.Process("", c)
+	if err != nil {
+		log.Printf("Error %v when parse config\n", err)
+		panic(err)
+	}
+	log.Printf("Config: %v\n", c)
+}
