20f34756c6a6851b084a5285516183cb0cfa37f4
glebnazemnov <gnazemnov@wimark.com>
2020-10-03 14:11:45 +0400

    Update MongoLib

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/gommon v0.3.0
 	golang.org/x/net v0.0.0-20200927032502-5d4f70055728 // indirect
 	golang.org/x/sys v0.0.0-20200929083018-4d22bbb62b3c // indirect
 )
--- internal/mongo/mongo.go
+++ internal/mongo/mongo.go
@@ -1,7 +1,9 @@
 package mongo
 
 import (
+	"fmt"
 	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"time"
 )
 
@@ -11,7 +13,11 @@ type MongoDB struct {
 	sess *mgo.Session
 }
 
-const connectionTimeout = 10 * time.Second
+const (
+	connectionTimeout = 10 * time.Second
+)
+
+const errIsNotConnected = "Mongo is not Connected"
 
 //NewConnection return new MongoDB connection
 //connection with url
@@ -29,4 +35,107 @@ func NewConnection(url string) (*MongoDB, error) {
 //IsConnected check connection to mongo db Server
 func (db *MongoDB) IsConnected() bool {
 	return db.sess != nil
+}
+
+//Insert insert document to collection
+//if collection is not created this function create collection
+func (db *MongoDB) Insert(coll string, v ...interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	return sess.DB("").C(coll).Insert(v...)
+}
+
+//Find find document in collection
+func (db *MongoDB) Find(coll string, query map[string]interface{}, v interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	bsonQuery := bson.M{}
+
+	for k, qv := range query {
+		bsonQuery[k] = qv
+	}
+
+	return sess.DB("").C(coll).Find(bsonQuery).All(v)
+}
+
+//FindByID find document by ID
+func (db *MongoDB) FindByID(coll string, id string, v interface{}) bool {
+	if !db.IsConnected() {
+		return false
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	return mgo.ErrNotFound != sess.DB("").C(coll).FindId(id).One(v)
+}
+
+//FindAll find all document in collection
+func (db *MongoDB) FindAll(coll string, v interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	return sess.DB("").C(coll).Find(bson.M{}).All(v)
+}
+
+//FindWithQuery you can call this function with query
+//you can must use mgo.bson format
+func (db *MongoDB) FindWithQuery(coll string, query interface{}, v interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	return sess.DB("").C(coll).Find(query).One(v)
+}
+
+//FindWithQueryAll you can find all document in collection with this function
+//you can call this function with mgo.bson query
+func (db *MongoDB) FindWithQueryAll(coll string, query interface{}, v interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	return sess.DB("").C(coll).Find(query).All(v)
+}
+
+//RemoveWithIDs delete all document in collection by ids
+func (db *MongoDB) RemoveWithIDs(coll string, ids interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	_, err := sess.DB("").C(coll).RemoveAll(bson.M{"_id": bson.M{"$in": ids}})
+
+	return err
+}
+
+//Update document by query
+//warning you can update all document with this query
+func (db *MongoDB) Update(coll string, query interface{}, set interface{}) error {
+	if !db.IsConnected() {
+		return fmt.Errorf("%s", errIsNotConnected)
+	}
+	var err error
+	var sess = db.sess.Copy()
+	defer sess.Close()
+
+	_, err = sess.DB("").C(coll).UpdateAll(query, set)
+
+	return err
 }
