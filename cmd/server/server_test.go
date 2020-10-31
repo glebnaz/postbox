f70c5797c2b46c80626104394b747e2a7d27ff87
glebnazemnov <gnazemnov@wimark.com>
2020-11-01 00:08:20 +0400

    Refactor tests

--- cmd/server/server_test.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "testing"
-
-//TestMiddleware check middleware
-//generate jwt, and check this jwt
-func TestMiddleware(t *testing.T) {
-
-}
--- test/secure/jwt_test.go
+++ test/secure/jwt_test.go
@@ -1,4 +1,4 @@
-package test
+package secure
 
 import (
 	"math/rand"
