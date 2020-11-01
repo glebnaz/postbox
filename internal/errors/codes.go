1878403a8f43c44f08d3f3b6f9005d13a4e631da
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-11-01 17:57:09 +0400

    Add code for Errors

---
+++ internal/errors/codes.go
@@ -0,0 +1,12 @@
+package errors
+
+const (
+	//DataBaseCode code for database error
+	DataBaseCode = 1
+	//SMTPCode code for SMTP error
+	SMTPCode = 2
+	//EntityCode code for consistent information error
+	EntityCode = 3
+	//InternalCode code for internal problems
+	InternalCode = 4
+)
